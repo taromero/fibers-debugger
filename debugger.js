c3632bdfff3e7dc98922d682b779f5186e27553f
Tomas Romero <canotto90@gmail.com>
2015-07-30 14:58:16 -0300

    Remove Inquirer dependency

--- debugger.js
+++ debugger.js
@@ -1,31 +1,13 @@
 function debug () {
-  var inquirer = Npm.require('inquirer')
   Future = Npm.require('fibers/future')
   var fut = new Future()
   var _s = Error().stack
-  // readline_prompt() // uncomment this to use Node's plain readline
-  inquirer.prompt({ type: 'input', name: 'text', message: 'repl' }, (repl).future())
+  readline_prompt() // uncomment this to use Node's plain readline
   fut.wait()
 
-  function repl (_input) {
-    var input = _input.text
-    if (input !== 'c') {
-      try {
-        console.log(eval(input))
-      } catch (e) {
-        console.log(input, 'is not defined.')
-      }
-      _debug()
-    }
-    fut.return()
-  }
-
-  function readline_prompt() {
+  function readline_prompt () {
     var readline = Npm.require('readline')
-    var rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    })
+    var rl = readline.createInterface({ input: process.stdin, output: process.stdout })
 
     rl.question('repl: ', (function (input) {
       if (input !== 'c') {
--- package.js
+++ package.js
@@ -1,13 +1,9 @@
 Package.describe({
   name: 'canotto90:fibers-debugger',
-  version: '0.0.4',
+  version: '0.0.5',
   summary: 'REPL with context on breakpoints, in the same terminal window where you run the fibers app.',
   git: 'https://github.com/taromero/fibers-debugger.git',
   documentation: 'README.md'
-})
-
-Npm.depends({
-  inquirer: '0.9.0'
 })
 
 Package.onUse(function (api) {
